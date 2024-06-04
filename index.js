318aae3c9041dbfd9abbae9aa878935ef02eb4c8
Taxis <111230411+Taxis8@users.noreply.github.com>
2024-06-05 00:58:33 +0300

    error supp

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const Eris = require("eris");
+window.onerror = stoperror;
 const keep_alive = require('./keep_alive.js')
+window.onerror = stoperror;
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
+window.onerror = stoperror;
 
 bot.on("error", (err) => {
+  window.onerror = stoperror;
   console.error(err); // or your preferred logger
 });
 
 bot.connect(); // Get the bot to connect to Discord
+window.onerror = stoperror;
+
+function stoperror() {
+   return true;
+}
